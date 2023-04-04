390e49eba1c8970ee0a736360aa5588931ac7c70
rnitin <rnitin@vmware.com>
2023-04-04 18:44:35 +0530

    refactoring

--- src/main/java/com/rvce/IotSensorsApplication.java
+++ src/main/java/com/rvce/IotSensorsApplication.java
@@ -1,5 +1,8 @@
 package com.rvce;
 
+import com.pi4j.Pi4J;
+import com.pi4j.util.Console;
+import com.rvce.controller.PrintInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +11,17 @@ public class IotSensorsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IotSensorsApplication.class, args);
+
+		final var console = new Console();
+
+		// Print program title/header
+		console.title("<-- The Pi4J Project -->", "Minimal Example project");
+
+		var pi4j = Pi4J.newAutoContext();
+
+		PrintInfo.printLoadedPlatforms(console, pi4j);
+		PrintInfo.printDefaultPlatform(console, pi4j);
+		PrintInfo.printProviders(console, pi4j);
 	}
 
 }
